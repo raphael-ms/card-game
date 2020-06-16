d08b60a271192517a3f8cc03a784a11cc2572b3b
raphael-ms <raphaelmarques77@gmail.com>
2020-06-16 13:17:18 -0300

    Add Auth methods and angular/fire

--- card-game/src/app/app.module.ts
+++ card-game/src/app/app.module.ts
@@ -9,6 +9,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: 'AIzaSyBNxz4pDeXiUNvpBurb0pRvw94NO_H4Q-Q',
+  authDomain: 'card-gamee.firebaseapp.com',
+  databaseURL: 'https://card-gamee.firebaseio.com',
+  projectId: 'card-gamee',
+  storageBucket: 'card-gamee.appspot.com',
+  messagingSenderId: '1091266771398',
+  appId: '1:1091266771398:web:db241497bdae033d135ac7',
+  measurementId: 'G-0KFR5HL7K4'
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +35,9 @@ import { MatMenuModule } from '@angular/material/menu';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatMenuModule
+    MatMenuModule,
+    AngularFireModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
